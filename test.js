739456016f10118a47738a52a1de5c643108e3fc
陈昊天 <2363822647@qq.com>
2022-12-19 22:30:09 +0800

    upd

--- test.js
+++ test.js
@@ -9,21 +9,21 @@ http.createServer(function (req, res) {
     req.on('end', function () {
         body = querystring.parse(body);
         res.writeHead(200, { 'Content-Type': 'text/html; charset=utf8' });
-        if (body.prompt) {
-            const { Configuration, OpenAIApi } = require("openai");
-            const configuration = new Configuration({
-                apiKey: process.env.OPENAI_API_KEY,
-            });
-            const openai = new OpenAIApi(configuration);
-            const response = openai.createCompletion({
-                model: "text-davinci-003",
-                prompt: "Say this is a test",
-                max_tokens: 7,
-                temperature: 0,
-            });
-            res.write(response);
-            res.write('<br>END');
-        }
+
+        const { Configuration, OpenAIApi } = require("openai");
+        const configuration = new Configuration({
+            apiKey: process.env.OPENAI_API_KEY,
+        });
+        const openai = new OpenAIApi(configuration);
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: "Say this is a test",
+            max_tokens: 7,
+            temperature: 0,
+        });
+        res.write(response);
+        res.write('<br>END');
+
         res.end();
     });
 }).listen(3000);
