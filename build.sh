f00629916aa2ec0214f029a5ef8e760e6e615852
陈昊天 <2363822647@qq.com>
2022-12-20 17:21:55 +0800

    upd

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-cd /var/www/html && rm -rf openai && git clone https://github.com/chen2438/openai.git && cd openai && npm i openai
+cd /var/www/html && rm -rf openai && git clone https://github.com/chen2438/openai.git && cd openai && npm i openai && npm i he
