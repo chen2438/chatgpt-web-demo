aa4b5ba11f3f4914fed46bcd9bea3c15400c3e72
陈昊天 <2363822647@qq.com>
2022-12-21 20:36:42 +0800

    upd

--- test.js
+++ test.js
@@ -1,39 +1,36 @@
 var http = require('http');
 var querystring = require('querystring');
-// var he = require('he');
+
+async function getOpenAI(body) {//OpenAI API
+    const { Configuration, OpenAIApi } = require("openai");
+    const configuration = new Configuration({
+        apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: body.prompt,
+        max_tokens: Number(body.max_tokens),
+        temperature: Number(body.temperature),
+    });
+    return response.data;
+}
 
 http.createServer(function (req, res) {
     var body = "";
-    req.on('data', function (chunk) {
+    req.on('data', function (chunk) {//接收表单参数
         body += chunk;
     });
     req.on('end', function () {
-        body = querystring.parse(body);
-        res.writeHead(200, {
+        body = querystring.parse(body);//解析参数
+        console.log("获取到POST参数:\n" + body);
+        res.writeHead(200, {//返回json格式, 允许跨域
             'Content-Type': 'application/json; charset=utf8', 'Access-Control-Allow-Origin': '*'
         });
-        async function f() {
-            console.log(body.prompt);
-            const { Configuration, OpenAIApi } = require("openai");
-            const configuration = new Configuration({
-                apiKey: process.env.OPENAI_API_KEY,
-            });
-            const openai = new OpenAIApi(configuration);
-            const response = await openai.createCompletion({
-                model: "text-davinci-003",
-                prompt: body.prompt,
-                max_tokens: Number(body.max_tokens),
-                temperature: Number(body.temperature),
-            });
-            try {
-                console.log(response.data);
-                res.write(JSON.stringify(response.data));
-                res.end();
-            } catch (err) {
-                console.log(err);
-            }
-        }
-        f();
+        var resData = getOpenAI(body);
+        console.log("返回值:\n" + resData);
+        res.write(JSON.stringify(resData));
+        res.end();
     });
 }).listen(3000);
 
