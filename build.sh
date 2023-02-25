05e7d6ef9d7531641bf7ec9985670887b9585718
chen2438 <2363822647@qq.com>
2023-02-25 20:22:21 +0800

    upd login

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
 cd /var/www/html && rm -rf chatgpt-web-demo && git clone https://github.com/chen2438/chatgpt-web-demo.git && cd chatgpt-web-demo && npm i openai && node server.js
+
+cd /var/www/html && rm -rf chatgpt-web-demo && git clone https://github.com/chen2438/chatgpt-web-demo.git && cd chatgpt-web-demo && npm i express cors && node login-server.js
